a73d2b3a754c5c28a68538099a8489c2efe33869
Mikkel Thestrup <mikkel@mithe.dk>
2026-03-03 14:21:20 +0100

    Fixed indents

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -3,33 +3,33 @@
 {
     fonts.packages = [
         (pkgs.iosevka.override {
-         set = "Custom";
-         privateBuildPlan = {
-         family = "Iosevka Custom";
-         spacing = "term";
-         serifs = "sans";
-         noCvSs = true;
-         exportGlyphNames = false;
+            set = "Custom";
+            privateBuildPlan = {
+                family = "Iosevka Custom";
+                spacing = "term";
+                serifs = "sans";
+                noCvSs = true;
+                exportGlyphNames = false;
 
-         variants = {
-         inherits = "ss20";
-         design = {
-         seven = "curly-serifless-crossbar";
-         capital-z = "curly-serifless-with-crossbar";
-         z = "curly-serifless-with-crossbar";
-         };
-         };
+                variants = {
+                    inherits = "ss20";
+                    design = {
+                        seven = "curly-serifless-crossbar";
+                        capital-z = "curly-serifless-with-crossbar";
+                        z = "curly-serifless-with-crossbar";
+                    };
+                };
 
-         weights = {
-         Regular = { shape = 400; menu = 400; css = 400; };
-         Bold    = { shape = 700; menu = 700; css = 700; };
-         };
+                weights = {
+                    Regular = { shape = 400; menu = 400; css = 400; };
+                    Bold    = { shape = 700; menu = 700; css = 700; };
+                };
 
-         slopes = {
-             Upright = { angle = 0;   shape = "upright"; menu = "upright"; css = "normal"; };
-             Italic  = { angle = 9.4; shape = "italic";  menu = "italic";  css = "italic"; };
-         };
-         };
+                slopes = {
+                    Upright = { angle = 0;   shape = "upright"; menu = "upright"; css = "normal"; };
+                    Italic  = { angle = 9.4; shape = "italic";  menu = "italic";  css = "italic"; };
+                };
+            };
         })
     ];
 }
